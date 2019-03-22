884a769655e591c71bee25ad045e792a7352256c
nileshpardeshi <nileshpardeshi2507@gmail.com>
2019-03-23 01:12:26 +0530

    new branch commit

--- src/sample1/Hello.java
+++ src/sample1/Hello.java
@@ -4,10 +4,13 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("my first commit");
+		System.out.println("my first commit using new branch");
 		
 		//developer 2 has added msg
 		System.out.println("my second commit");
+		
+		//changes on new branch
+		System.out.println("new branch added this line");
 
 	}
 
