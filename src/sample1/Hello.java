ab2acb40d016bc3eccac2ff9241ed4a1b3aeae60
nileshpardeshi <nileshpardeshi2507@gmail.com>
2019-03-22 23:56:02 +0530

    my second commit

--- src/sample1/Hello.java
+++ src/sample1/Hello.java
@@ -5,6 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("my first commit");
+		
+		//developer 2 has added msg
+		System.out.println("my second commit");
 
 	}
 
